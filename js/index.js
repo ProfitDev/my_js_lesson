97cbb5538753ab1f4b9e2907905ab8c9bd2f43cd
ProfitDev <szhylyuk84@gmail.com>
2021-02-15 14:00:39 +0200

    update

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const message = 'Hello JS!';
+alert(message);
+
+const userName = prompt('Enter your name');
+alert('Hello ' + userName);
+
+const userAge = prompt('Enter your age');
+alert('Your age is ' + userAge);
+
+const nextAge = userAge + 1;
+alert('Next year you will be ' + userAge++);
